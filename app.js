4d10fa6be00b5b0528c7d9e86ec5c94f4b06d015
Bhumika gupta <bg0674079@gmail.com>
2024-04-20 18:16:58 +0530

    boilerplate

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 require("dotenv").config({path : "./.env"});
 const express = require('express');
 const app = express();
+const { homepage } = require("./controllers/indexController");
 
-app.listen(
-	process.env.PORT , 
-	console.log(`server running onnection port ${process.env.PORT}`)
-)
+//logger 
+const logger = require('morgan');
+
+app.use(logger("tiny"));
+
+app.use("/", homepage)
+
+app.listen(process.env.PORT, () => {
+    console.log(`server running on port ${process.env.PORT}`);
+});
